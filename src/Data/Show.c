3421a098e6650b3d914b7e6a6c5b3ddeaae2658e
Felix Schlitter <felixschlitter@protonmail.com>
2018-08-26 22:44:55 +1200

    Implement C FFI for Data.Show and Record.Unsafe

--- src/Data/Show.c
+++ src/Data/Show.c
@@ -16,39 +16,69 @@ PURS_FFI_FUNC_DEF(Data_Show_showStringImpl, x, {
 		afmt("\"%s\"", purs_any_get_string(x)->data));
 })
 
-PURS_FFI_FUNC_DEF(Data_Show_showArrayImpl, f, {
-	return PURS_FFI_LAMBDA(xs, {
-		const purs_vec_t * zs = purs_any_get_array(xs);
-		const purs_any_t * tmp;
-		const managed_utf8str_t * tmp_s;
-		int i;
-		char * out;
-		char * tmp_out;
+// TODO: Implement https://github.com/purescript-c/purescript-prelude/blob/a878e8d9531cf8c549ef46dfce16988380792cc2/src/Data/Show.js#L12-L27
+PURS_FFI_FUNC_DEF(Data_Show_showCharImpl, x, {
+	return PURS_ANY_STRING(
+		afmt("'%s'", purs_any_get_string(x)->data));
+})
 
-		purs_vec_foreach(zs, tmp, i) {
-			tmp_s = purs_any_get_string(purs_any_app(f, tmp));
-			tmp_out = out;
+PURS_FFI_FUNC_DEF_2(Data_Show_showArrayImpl, f, xs, {
+	const purs_vec_t * zs = purs_any_get_array(xs);
+	const purs_any_t * tmp;
+	const managed_utf8str_t * tmp_s;
+	int i;
+	char * out = NULL;
+	char * tmp_out;
+	purs_vec_foreach(zs, tmp, i) {
+		tmp_s = purs_any_get_string(purs_any_app(f, tmp));
+		tmp_out = out;
 
-			if (i == 0) {
-				if (i == zs->length - 1) {
-					out = afmt("[%s]", tmp_s->data);
-				} else {
-					out = afmt("[%s, ", tmp_s->data);
-				}
+		if (i == 0) {
+			if (i == zs->length - 1) {
+				out = afmt("[%s]", tmp_s->data);
 			} else {
-				if (i == zs->length - 1) {
-					out = afmt("%s%s]", out, tmp_s->data);
-				} else {
-					out = afmt("%s%s, ", out, tmp_s->data);
-				}
+				out = afmt("[%s, ", tmp_s->data);
 			}
-
-			if (tmp_out != NULL) {
-				// XXX: figure this out
-				// free(tmp_out);
+		} else {
+			if (i == zs->length - 1) {
+				out = afmt("%s%s]", out, tmp_s->data);
+			} else {
+				out = afmt("%s%s, ", out, tmp_s->data);
 			}
 		}
 
-		return PURS_ANY_STRING(out);
-	});
+		if (tmp_out != NULL) {
+			free(tmp_out);
+		}
+	}
+
+	return PURS_ANY_STRING(out);
+})
+
+PURS_FFI_FUNC_DEF_2(Data_Show_cons, a, xs, {
+	return PURS_ANY_ARRAY(
+		purs_vec_insert(purs_any_get_array(xs),
+				0,
+				a));
+})
+
+PURS_FFI_FUNC_DEF_2(Data_Show_join, a, xs, {
+	const purs_vec_t * zs = purs_any_get_array(xs);
+	const char * sep = purs_any_get_string(a)->data;
+	const purs_any_t * tmp;
+	int i;
+	char * out = NULL;
+	char * tmp_out;
+	purs_vec_foreach(zs, tmp, i) {
+		tmp_out = out;
+		out = afmt("%s%s%s",
+			   out == NULL ? "" : out,
+			   out == NULL ? "" : sep,
+			   purs_any_get_string(tmp)->data);
+
+		if (tmp_out != NULL) {
+			free(tmp_out);
+		}
+	}
+	return PURS_ANY_STRING(out);
 })
---
+++ src/Record/Unsafe.c
@@ -0,0 +1,31 @@
+#include "runtime/purescript.h"
+
+PURS_FFI_FUNC_DEF_2(Record_Unsafe_unsafeHas, key, x, {
+	const purs_record_t * rec = purs_any_get_record(x);
+	const void * key_ = purs_any_get_string(key)->data;
+	const purs_record_t * val = purs_record_find_by_key(rec, key_);
+	return val != NULL;
+})
+
+PURS_FFI_FUNC_DEF_2(Record_Unsafe_unsafeGet, key, x, {
+	const purs_record_t * rec = purs_any_get_record(x);
+	const void * key_ = purs_any_get_string(key)->data;
+	const purs_record_t * val = purs_record_find_by_key(rec, key_);
+	if (val != NULL) {
+		return val->value;
+	} else {
+		return (const purs_any_t *) NULL;
+	}
+})
+
+PURS_FFI_FUNC_DEF_3(Record_Unsafe_unsafeSet, key, value, x, {
+	const purs_record_t * rec = purs_any_get_record(x);
+	const void * key_copy = purs_string_copy(purs_any_get_string(key)->data);
+	return purs_record_add(rec, key_copy, value);
+})
+
+PURS_FFI_FUNC_DEF_2(Record_Unsafe_unsafeDelete, key, x, {
+	const purs_record_t * rec = purs_any_get_record(x);
+	const void * key_ = purs_any_get_string(key)->data;
+	return PURS_ANY_RECORD(purs_record_remove(rec, key_));
+})
