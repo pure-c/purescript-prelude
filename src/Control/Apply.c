5986a0b5bb2e0521e666930ffa69474616a126f6
Felix Schlitter <felixschlitter@protonmail.com>
2018-08-29 09:20:13 +1200

    Implement Control.Apply

---
+++ src/Control/Apply.c
@@ -0,0 +1,20 @@
+#include "runtime/purescript.h"
+
+PURS_FFI_FUNC_2(Control_Apply_arrayApply, _fs, _xs, {
+	const purs_vec_t * fs = purs_any_get_array(_fs);
+	const purs_vec_t * xs = purs_any_get_array(_xs);
+	purs_vec_t * out = (purs_vec_t *) purs_vec_new();
+	purs_vec_reserve(out, fs->length * xs->length);
+
+	int n = 0;
+	const purs_any_t * f;
+	for (int i = 0; i < fs->length; i++) {
+		const purs_any_t * f = fs->data[i];
+		for (int j = 0; j < xs->length; j++) {
+			out->data[n++] = purs_any_app(f, xs->data[j]);
+		}
+	}
+	out->length = n;
+
+	return PURS_ANY_ARRAY(out);
+})
---
+++ src/Control/Apply.h
@@ -0,0 +1,8 @@
+#ifndef Control_Apply_H
+#define Control_Apply_H
+
+#include "runtime/purescript.h"
+
+PURS_FFI_DECL(Control_Apply_arrayApply)
+
+#endif // Control_Apply_H
