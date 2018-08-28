cc3afd719cb1250d3e8bc178cc7227fd8f5729f7
Felix Schlitter <felixschlitter@protonmail.com>
2018-08-28 21:49:04 +1200

    Implement Data.Functor C FFI

---
+++ src/Data/Functor.c
@@ -0,0 +1,12 @@
+#include "runtime/purescript.h"
+
+PURS_FFI_FUNC_2(Data_Functor_arrayMap, f, _xs, {
+	const purs_vec_t * xs = purs_any_get_array(_xs);
+	purs_vec_t * copy = (purs_vec_t *) purs_vec_copy(xs);
+	int i;
+	const purs_any_t * tmp;
+	purs_vec_foreach(copy, tmp, i) {
+		copy->data[i] = purs_any_app(f, tmp);
+	}
+	return PURS_ANY_ARRAY((const purs_vec_t *) copy);
+})
---
+++ src/Data/Functor.h
@@ -0,0 +1,8 @@
+#ifndef Data_Functor_H
+#define Data_Functor_H
+
+#include "runtime/purescript.h"
+
+PURS_FFI_DECL(Data_Functor_arrayMap)
+
+#endif // Data_Functor_H
