4cbf0527de196a45ec8d01ca344d5bff40c485c8
Felix Schlitter <felixschlitter@protonmail.com>
2018-08-29 12:07:40 +1200

    Implement Control.Bind

---
+++ src/Control/Bind.c
@@ -0,0 +1,18 @@
+#include "runtime/purescript.h"
+
+PURS_FFI_FUNC_2(Control_Bind_arrayBind, _xs, f, {
+	int i;
+	const purs_any_t * tmp;
+	const purs_vec_t * xs = purs_any_get_array(_xs);
+	purs_vec_t * out = (purs_vec_t *) purs_vec_new();
+	purs_vec_foreach(xs, tmp, i) {
+		int k;
+		const purs_any_t * tmp_2;
+		const purs_any_t * u = purs_any_app(f, tmp);
+		const purs_vec_t * v = purs_any_get_array(u);
+		purs_vec_foreach(v, tmp_2, k) {
+			purs_vec_push_mut(out, tmp_2);
+		}
+	}
+	return PURS_ANY_ARRAY((const purs_vec_t *) out);
+})
---
+++ src/Control/Bind.h
@@ -0,0 +1,8 @@
+#ifndef Control_Bind_H
+#define Control_Bind_H
+
+#include "runtime/purescript.h"
+
+PURS_FFI_DECL(Control_Bind_arrayBind)
+
+#endif // Control_Bind_H
