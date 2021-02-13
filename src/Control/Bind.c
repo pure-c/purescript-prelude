ae5f9151cb89b23187352feedc5c4ceb6eb0c18a
Felix Schlitter <felixschlitter@gmail.com>
2021-02-14 10:44:06 +1300

    Fix leak in array bind impl

--- src/Control/Bind.c
+++ src/Control/Bind.c
@@ -12,10 +12,12 @@ PURS_FFI_FUNC_2(Control_Bind_arrayBind, _xs, f) {
 	purs_vec_t *out = (purs_vec_t *) purs_vec_new();
 	purs_vec_foreach(xs, tmp, i) {
 		int k;
-		purs_any_t tmp_2;
-		const purs_vec_t *v = purs_any_force_array(purs_any_app(f, tmp));
-		purs_vec_foreach(v, tmp_2, k) {
-			purs_vec_push_mut(out, tmp_2);
+		purs_any_t tmp2, tmp1;
+		tmp1 = purs_any_app(f, tmp);
+		const purs_vec_t *v = purs_any_force_array(tmp1);
+		PURS_ANY_RELEASE(tmp1);
+		purs_vec_foreach(v, tmp2, k) {
+			purs_vec_push_mut(out, tmp2);
 		}
 		PURS_RC_RELEASE(v);
 	}
